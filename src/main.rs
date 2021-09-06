331097199c25460d65f36e411e670e15775a90ed
pessonnier <loic.pessonnier@gmail.com>
2021-09-06 22:09:07 +0200

    ca bloque encore sur le read

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::io::{stdin, Read, Write};
+use std::io::{stdin, stdout, Read, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::str::from_utf8;
 use std::thread;
 
 // utiliser clap
+const BUFFSIZE_CLIENT: usize = 6;
+const BUFFSIZE_SERVER: usize = 50;
 
 fn server0() {
     println!("Server");
@@ -22,12 +24,18 @@ fn server0() {
 
 fn traite_client(mut stream: TcpStream) {
     //mut stream: TcpStream
-    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    let client_tcp = stream.peer_addr().unwrap().to_string();
+    let mut data = [0 as u8; BUFFSIZE_SERVER]; // using 50 byte buffer
+    println!("flush {}", client_tcp);
+    stream.flush().unwrap();
+    println!("<...");
     while match stream.read(&mut data) {
         Ok(size) => {
             data.reverse(); // let t = s.chars().rev().collect::<String>();
-            stream.write(&data[50 - size..]).unwrap();
-            print!("{}", from_utf8(&data[50 - size..]).unwrap());
+            println!("{} > {}", client_tcp, from_utf8(&data).unwrap());
+            stream.write(&data[BUFFSIZE_SERVER - size - 1..]).unwrap();
+            stream.flush().unwrap();
+            println!("<...");
             true
         }
         Err(_) => {
@@ -59,15 +67,62 @@ fn server() {
     }
 }
 
+fn client_lecture0(stream: &mut TcpStream) {
+    let mut rbuf = [0u8; BUFFSIZE_CLIENT]; //Vec<u8> = Vec::new();
+    loop {
+        let lu_stream = stream.read(&mut rbuf).unwrap();
+        if lu_stream == 0 {
+            break;
+        }
+        println!("lu stream {}", lu_stream);
+        println!("server > {}", from_utf8(&rbuf).unwrap());
+    }
+}
+
+fn client_lecture1(stream: &mut TcpStream) {
+    let mut rbuf: Vec<u8> = Vec::new();
+    let lu_stream = stream.read_to_end(&mut rbuf).unwrap();
+    if lu_stream == 0 {
+        println!("pas de message");
+    }
+    println!("lu stream {}", lu_stream);
+    println!("server > {}", from_utf8(&rbuf).unwrap());
+}
+
+fn client_lecture(stream: &mut TcpStream) {
+    let mut data = [0u8; BUFFSIZE_CLIENT];
+    while match stream.read(&mut data) {
+        Ok(0) => false,
+        Ok(size) => {
+            println!("lu stream {}", size);
+            println!("server > {}", from_utf8(&data).unwrap());
+            true
+        }
+        Err(e) => {
+            println!("cnx err : {}", e);
+            false
+        }
+    } {}
+}
+
 fn client() {
     match TcpStream::connect("127.0.0.1:8888") {
         Ok(mut stream) => {
             println!("cnx ok {}", stream.peer_addr().unwrap());
+            stream.flush().unwrap();
+            //stream.set_nodelay(true);
             loop {
-                let mut buf = [0u8; 6];
-                stdin().read(&mut buf).unwrap();
-                // let car = from_utf8(&buf).unwrap();
-                stream.write(&buf).unwrap();
+                let mut wbuf = [0u8; BUFFSIZE_CLIENT];
+                print!("> ");
+                stdout().flush().unwrap();
+                let lu_stdin = stdin().read(&mut wbuf).unwrap();
+                //println!();
+                println!("lu stdin {}", lu_stdin);
+                // let car = from_utf8(&wbuf).unwrap();
+                stream.write_all(&wbuf).unwrap();
+                stream.flush().unwrap();
+                println!("<...");
+                client_lecture(&mut stream);
             }
         }
         Err(e) => {
